55e102239da26862e986bcb58ee1b84e013de25c
LiamMarega <liammarega85@gmail.com>
2022-11-28 19:54:44 -0300

    Fix texts

--- src/pages/Home/parts/Carousel/index.jsx
+++ src/pages/Home/parts/Carousel/index.jsx
@@ -9,16 +9,19 @@ import boat3 from '../../../../assets/images/3.png';
 import boat4 from '../../../../assets/images/4.png';
 import boat5 from '../../../../assets/images/5.png';
 
+import MovingText from 'react-moving-text';
+
 function Carousel() {
   return (
     <div className="ContainerCarouselComponent">
       <Grid container>
         <Grid md={5}>
           <div className="containerText">
-            <div className="text">
+            <div className="text ">
               <h1>LUXURY LIFESTYLE RENTALS</h1>
-              <p>Vacay mood</p>
-              <Button variant="outlined" sx={{padding: ' 3% 7%'}}>
+
+              <MovingText type="typewriter" dataText={['Vacay  ', 'Mood   ', ';)         ']} />
+              <Button variant="outlined" sx={{padding: ' 3% 7%', margin: '5% 0'}}>
                 Reserve now
               </Button>
             </div>
